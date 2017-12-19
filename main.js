59b2dbbee2bd3f405664858ebab72baa1403f19c
dauglyon <dauglyon@users.noreply.github.com>
2017-12-19 15:58:38 -0500

    Added login options to form

--- main.js
+++ main.js
@@ -14,35 +14,58 @@ $(document).ready(function(){
     params = {"studyDbIds" : [form.study], "observationLevel" : form.unit};
     loadBrAPIData(form.server,params,function(response){
       useBrAPIData(response,(!!form.group));
-    });
+    },form.username,form.password);
     return false;
   })
 });
 
-function loadBrAPIData(server,parameters,success){
-  url = server;
-  if (url.slice(0,8)!="https://" && url.slice(0,7)!="http://"){
-    url ="http://"+url;
+function loadBrAPIData(server,parameters,success,username,password){
+  var base_url = server;
+  if (base_url.slice(0,8)!="https://" && base_url.slice(0,7)!="http://"){
+    base_url ="http://"+base_url;
   }
-  url+= "/brapi/v1/phenotypes-search";
-  data = {
+  if (base_url.slice(-1)!="/"){
+    base_url+="/";
+  }
+  
+  var load_url = base_url+"brapi/v1/phenotypes-search";
+  var data = {
     "pageSize" : 10000000,
     "page" : 0
   };
   d3.entries(parameters).forEach(function(entry){
     data[entry.key] = data[entry.key]||entry.value;
   });
-  $.ajax({
-    type: "POST",
-    url: url,
-    data: data,
-    success: success,
-  });
+  
+  if(username){
+    $.ajax({
+      type: "POST",
+      url: base_url+"brapi/v1/token",
+      data: {'username':username,'password':password},
+      success: function(response){
+        data["access_token"] = response.access_token;
+        load();
+      },
+    });
+  }
+  else {
+    load();
+  }
+  
+  function load(){
+    $.ajax({
+      type: "POST",
+      url: load_url,
+      data: data,
+      success: success,
+    });
+  }
 };
 
 // filters and modifies the response and then creates the root filter object
 // and datatable
 function useBrAPIData(response,groupByAccession){
+  console.log(response);
   var traits = {};
   var data = response.result.data
     .map(function(observeUnit){
@@ -71,7 +94,6 @@ function useBrAPIData(response,groupByAccession){
     {title:"Name",data:"observationUnitName"},
     {title:"Accession",data:"germplasmName"},
   ];
-  console.log(data);
   if (groupByAccession){
     var grouped = d3.nest().key(function(d){return d.germplasmDbId}).entries(data);
     var newdata = grouped.map(function(group){
@@ -90,7 +112,6 @@ function useBrAPIData(response,groupByAccession){
       });
       return newDatum;
     });
-    console.log(newdata);
     var tableCols = [
       {title:"Accession", data:"germplasmName"},
       {title:"Unit Count", data:"count"}
